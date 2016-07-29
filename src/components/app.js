02fae00beeba8f6f9c8982ef7bd4b90147c3e6d1
Ilya Ivanov <static.ila@gmail.com>
2016-07-29 19:35:50 +0300

    Initial commit based on slingshot starter kit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Core extends React.Component {
+    render() {
+        return (<div>
+            <h1>Hello World from True Tune</h1>
+            <h3>Powered by a Slingshot</h3>
+        </div>);
+    }
+}
+
+export default Core;
