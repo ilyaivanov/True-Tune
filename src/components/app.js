4e0c765c58fbd5ba68e73e4a8720a743bea252c7
Ilya Ivanov <static.ila@gmail.com>
2016-07-30 13:33:58 +0300

    Added naive implementation to search for artists via lastfm

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import Select2 from 'react-select2-wrapper';
+import 'react-select2-wrapper/css/select2.css';
 
 class Core extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            artists: [
+            ]
+        };
+
+    }
+
     render() {
+        function select(p) {
+            var newState = this.state;
+            newState.artists.push(p.params.data);
+            this.setState(newState);
+        }
+        var configuration = {
+            ajax: {
+                url: function (params) {
+                    return "http://ws.audioscrobbler.com/2.0?method=artist.search&api_key=185032d80f1827034396b9acfab5a79f&format=json&artist=" + params.term;
+                },
+                processResults: function (data) {
+                    var artists = data.results.artistmatches.artist.map(a => ({text: a.name, id: a.mbid}));
+                    return {
+                        results: artists.slice(0, 25)
+                    };
+                }
+            },
+            delay: 250,
+            minimumInputLength: 1
+
+        };
         return (<div>
-            <h1>Hello World from True Tune</h1>
-            <h3>Powered by a Slingshot</h3>
+            <Select2
+                className="mySelect"
+                style={{width: '100%'}}
+                onSelect={select.bind(this)}
+                options={configuration}
+            />
+
+            <div>
+                {this.state.artists.map(a => <div key={a.id}>{a.text}</div>)}
+            </div>
         </div>);
     }
 }
