ff911e6c5f7999fa5bcb0a0ddd448111ca647a16
Ilya Ivanov <static.ila@gmail.com>
2016-08-03 02:48:26 +0300

    Remove old, redundant code

--- src/components/app.js
+++ src/components/app.js
@@ -4,105 +4,30 @@ import lastfm from './../loaders/lastfm';
 import Sidebar from './sidebar/sidebar';
 import {FormControl} from 'react-bootstrap';
 import SearchResults from './searchResults';
-import './../../node_modules/bootstrap/dist/css/bootstrap.css'
+import './../../node_modules/bootstrap/dist/css/bootstrap.css';
 
-class Core extends React.Component {
+class App extends React.Component {
     constructor(props) {
         super(props);
-
-        var tracks = [
-            {name: "Track 1", id: "111"},
-            {name: "Track 2", id: "112"},
-            {name: "Track 3", id: "113"},
-            {name: "Track 4", id: "114"},
-        ];
-
-        var albums = [
-            {name: "First Album", id: "11", tracks: tracks},
-            {name: "Second Album", id: "12", tracks: tracks},
-            {name: "Third Album", id: "13", tracks: tracks},
-        ];
-        this.state = {
-            artists: [
-                {name: 'First Artist', id: "1", albums: albums},
-                {name: 'Second Artist', id: "2", albums: albums},
-                {name: 'Third Artist', id: "3", albums: albums},
-            ]
-        };
-
+        this.state = {artists: []};
     }
 
     render() {
-        function select(p) {
-            var newState = this.state;
-            newState.artists.push(p.params.data);
-            this.setState(newState);
-        }
-
-        var configuration = {
-            ajax: {
-                url: function (params) {
-                    return "http://ws.audioscrobbler.com/2.0?method=artist.search&api_key=185032d80f1827034396b9acfab5a79f&format=json&artist=" + params.term;
-                },
-                processResults: function (data) {
-                    var artists = data.results.artistmatches.artist.map(a => ({text: a.name, id: a.id}));
-                    return {
-                        results: artists.slice(0, 25)
-                    };
-                }
-            },
-            delay: 250,
-            minimumInputLength: 1
-
-        };
-
-        function toggleAlbums(artist) {
-            var self = this;
-            artist.isAlbumsVisible = !artist.isAlbumsVisible;
-            if (!artist.albums) {
-                lastfm.findAlbums(artist.text)
-                    .then(function (albums) {
-                        artist.albums = albums;
-                        self.forceUpdate();
-                    });
-            } else {
-                this.forceUpdate();
-            }
-        }
-
-
-        function toggleTracks(album) {
-            var self = this;
-            album.isTracksVisible = !album.isTracksVisible;
-            if (!album.tracks) {
-                lastfm.findTracks(album)
-                    .then(function (tracks) {
-                        album.tracks = tracks;
-                        self.forceUpdate();
-                    });
-            } else {
-                this.forceUpdate();
-            }
-        }
-
-        function onChange(event, e) {
+        function onChange(event) {
             //TODO: add a delay
-            lastfm.findArtists(event.target.value)
-                .then(artists => this.setState({artists:artists}));
+            lastfm
+                .findArtists(event.target.value)
+                .then(artists => this.setState({artists: artists}));
         }
-
-        var self = this;
 
         function toggleArtist(artist) {
             artist.areAlbumsShown = !artist.areAlbumsShown;
-            console.log(artist);
-            self.forceUpdate();
+            this.forceUpdate();
         }
 
         function toggleAlbum(album) {
             album.areTracksShown = !album.areTracksShown;
-            console.log(album);
-            self.forceUpdate();
+            this.forceUpdate();
         }
 
         return (<div id="wrapper">
@@ -117,8 +42,8 @@ class Core extends React.Component {
                                 onChange={onChange.bind(this)}
                             />
                             <br/>
-                            <SearchResults toggleArtist={toggleArtist}
-                                           toggleAlbum={toggleAlbum}
+                            <SearchResults toggleArtist={toggleArtist.bind(this)}
+                                           toggleAlbum={toggleAlbum.bind(this)}
                                            artists={this.state.artists}/>
                         </div>
                     </div>
@@ -128,4 +53,4 @@ class Core extends React.Component {
     }
 }
 
-export default Core;
+export default App;
