baeb6cf9878b4e9934cff51daad2862a29906457
Ilya Ivanov <static.ila@gmail.com>
2016-08-03 11:52:21 +0300

    Take care of items with no id and duplicated ids
    For artists, albums and tracks

--- src/loaders/lastfm.js
+++ src/loaders/lastfm.js
@@ -1,16 +1,63 @@
 import $ from 'jquery';
-class lastfm  {
+class lastfm {
+    static findArtists(term) {
+        console.log('http request to lastfm. artist.search(\'' + term + '\')');
+        return $
+            .get("http://ws.audioscrobbler.com/2.0?method=artist.search&api_key=185032d80f1827034396b9acfab5a79f&format=json&artist=" + term)
+            .then(response => response.results.artistmatches.artist.map(a => ({name: a.name, id: a.mbid})))
+            .then(artists => this.validateItems(artists, 'artists'));
+    }
+
     static findAlbums(artistName) {
-        return $.get('http://ws.audioscrobbler.com/2.0?method=artist.getTopAlbums&api_key=185032d80f1827034396b9acfab5a79f&format=json&artist='+artistName)
-            .then(response => response.topalbums.album);
+        return $
+            .get('http://ws.audioscrobbler.com/2.0?method=artist.getTopAlbums&api_key=185032d80f1827034396b9acfab5a79f&format=json&artist=' + artistName)
+            .then(response => response.topalbums.album)
+            .then(albums => this.validateItems(albums, 'albums'));
     }
 
     static findTracks(album) {
         var artistName = album.artist.name;
         var albumName = album.name;
-        return $.get('http://ws.audioscrobbler.com/2.0?method=album.getInfo&api_key=185032d80f1827034396b9acfab5a79f&format=json&artist='+artistName+'&album='+albumName)
-            .then(response => response.topalbums.album);
+        return $
+            .get('http://ws.audioscrobbler.com/2.0?method=album.getInfo&api_key=185032d80f1827034396b9acfab5a79f&format=json&artist=' + artistName + '&album=' + albumName)
+            .then(response => response.topalbums.album)
+            .then(tracks => this.validateItems(tracks, 'tracks'));
     }
+
+
+    static validateItems(items, setName) {
+        items = items.filter(a => a.id);
+
+        var duplicated = this.getDuplicated(items, 'id');
+        if (duplicated) {
+            console.warn(`Found duplicated ${setName}\r\n` + duplicated);
+            console.warn('Taking the first artist by id');
+            items = this.filterOutDuplicatedBy(items, 'id')
+        }
+        return items;
+    }
+
+    static getDuplicated(items, targetPropertyName) {
+        var duplicated = _.chain(items)
+            .groupBy(i => i[targetPropertyName])
+            .toPairs()
+            .filter(pair => pair[1].length > 1)
+            .map(pair => pair[0] + ' : [' + pair[1].map(i => i.name).join(', ') + ']')
+            .join('\r\n')
+            .value();
+        return duplicated;
+    }
+
+    static filterOutDuplicatedBy(items, propertyName) {
+        return _.chain(items)
+            .groupBy(i => i[propertyName])
+            .toPairs()
+            .map(pair => pair[1][0])
+            .value();
+    }
+
+    //Check unique mbid in all entities
+    //probably use a different source of ids
 }
 
 export default lastfm;
