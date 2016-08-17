fcf745f5fe4473debd3e85c94069db53f24bd913
Ilya Ivanov <static.ila@gmail.com>
2016-08-17 22:14:25 +0300

    Removed redundant code

--- src/components/app.js
+++ src/components/app.js
@@ -26,30 +26,11 @@ class App extends React.Component {
 
     render() {
         var currentPlaylist = PlaylistsModel.getSelectedPlaylists();
-        let styles = {'marginBottom': 0};
         let page = currentPlaylist ? <Playlist playlist={currentPlaylist}/> :
             <SearchPage artists={ArtistsModel.artists}
                         playlists={PlaylistsModel.getPlaylists()}/>;
 
         return (<div>
-            {/*<nav className="navbar navbar-default navbar-static-top" role="navigation" style={styles}>*/}
-                {/*<div className="navbar-header">*/}
-                    {/*<button type="button" className="navbar-toggle" data-toggle="collapse"*/}
-                            {/*data-target=".navbar-collapse">*/}
-                        {/*<span className="sr-only">Toggle navigation</span>*/}
-                        {/*<span className="icon-bar"></span>*/}
-                        {/*<span className="icon-bar"></span>*/}
-                        {/*<span className="icon-bar"></span>*/}
-                    {/*</button>*/}
-                    {/*<a className="navbar-brand" href="JavaScript:;">True Tune</a>*/}
-                {/*</div>*/}
-
-                {/*<Player songInfo={PlayerModel.getCurrentTrackState()}*/}
-                        {/*isPlaying={PlayerModel.isPlaying}/>*/}
-
-                {/*<Sidebar playlists={PlaylistsModel.getPlaylists()}/>*/}
-
-            {/*</nav>*/}
             <nav className="navbar navbar-default navbar-fixed-top">
                 <div className="container-fluid">
                     <div className="navbar-header">
@@ -61,7 +42,10 @@ class App extends React.Component {
                         </button>
                         <a className="navbar-brand" href="#">Brand</a>
                         <button id="hide-playlists"
-                                className={classnames('navbar-btn btn btn-default glyphicon', {'glyphicon-arrow-left':UI.arePlaylistsVisible, 'glyphicon-arrow-right':!UI.arePlaylistsVisible})}
+                                className={classnames('navbar-btn btn btn-default glyphicon', {
+                                    'glyphicon-arrow-left': UI.arePlaylistsVisible,
+                                    'glyphicon-arrow-right': !UI.arePlaylistsVisible
+                                })}
                                 onClick={() => UI.togglePlaylists()}></button>
                     </div>
 
@@ -70,12 +54,16 @@ class App extends React.Component {
 
                         <ul className="nav navbar-nav">
 
-                            <li className="active"><a href="JavaScript:;" onClick={e => PlaylistsModel.selectPlaylist(undefined)}>Search</a></li>
+                            <li className="active"><a href="JavaScript:;"
+                                                      onClick={e => PlaylistsModel.selectPlaylist(undefined)}>Search</a>
+                            </li>
                         </ul>
                         <Player songInfo={PlayerModel.getCurrentTrackState()} isPlaying={PlayerModel.isPlaying}/>
                         <ul className="nav navbar-nav navbar-right">
                             <li className="dropdown">
-                                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span className="glyphicon glyphicon-cog"></span> <span className="caret"></span></a>
+                                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button"
+                                   aria-haspopup="true" aria-expanded="false"><span
+                                    className="glyphicon glyphicon-cog"></span> <span className="caret"></span></a>
                                 <ul className="dropdown-menu">
                                     <li><a href="#">Action</a></li>
                                     <li><a href="#">Another action</a></li>
@@ -89,7 +77,7 @@ class App extends React.Component {
                 </div>
             </nav>
             <div className="container-fluid">
-                <div className={classnames('row', {'no-playlists' : !UI.arePlaylistsVisible})}>
+                <div className={classnames('row', {'no-playlists': !UI.arePlaylistsVisible})}>
                     <div className="col-fixed-240">
                         <Sidebar playlists={PlaylistsModel.getPlaylists()}/>
                     </div>
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -39,49 +39,4 @@ let Sidebar = props => (
     </div>
 );
 
-let Sidebar1 = props => {
-    return (<div className="navbar-default sidebar" role="navigation">
-        <div className="sidebar-nav navbar-collapse">
-            <ul className="nav" id="side-menu">
-                <li className="sidebar-search">
-                    <div className="input-group custom-search-form">
-                        <input type="text" className="form-control" placeholder="Search..."/>
-                        <span className="input-group-btn">
-
-                                <button className="btn btn-default" type="button"
-                                        onClick={e => PlaylistsModel.selectPlaylist(undefined)}>
-                                <span className="glyphicon glyphicon-search"></span>
-                                </button>
-                            </span>
-                    </div>
-                </li>
-                <li>
-                    <a href="JavaScript:;" onClick={PlaylistsModel.createPlaylist}><i
-                        className="glyphicon glyphicon-plus"></i>
-                        Create playlist</a>
-                </li>
-                {props.playlists.map((p, i) =>
-                    (<li key={i}>
-                        {
-                            (p.isEditing) ?
-                                (<input type="text"
-                                        value={p.name}
-                                        onChange={event => PlaylistsModel.setPlaylistName(event, p)}
-                                        onBlur={() => PlaylistsModel.stopEditingPlaylist(p)}/>) :
-                                (
-                                    <a href="JavaScript:;" onClick={()=>PlaylistsModel.selectPlaylist(p)}><i
-                                        className="glyphicon glyphicon-list-alt"></i> {p.name}
-                                    </a>
-                                )
-                        }
-                        {!p.isEditing && (<button className="edit glyphicon glyphicon-edit"
-                                                  onClick={() => PlaylistsModel.editPlaylist(p)}></button>)}
-
-                    </li>))}
-            </ul>
-        </div>
-    </div>);
-};
-
-
 export default Sidebar;
