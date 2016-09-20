3138115f8519f095e9921b87eb9366815d3a4c91
Ilya Ivanov <static.ila@gmail.com>
2016-09-20 21:27:09 +0300

    Search page content draft. Including SCSS for layout for results, tabs and input boxes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './components/Layout.scss';
 
+import SearchPageContent from './components/SearchPageContent';
+
 class App extends React.Component {
     render() {
         return <main className="page-content">
@@ -8,7 +10,7 @@ class App extends React.Component {
                 <h1>Navigation</h1>
             </nav>
             <article className="content-article">
-                <h1>Main Content</h1>
+                <SearchPageContent/>
             </article>
             <aside className="content-sidebar">
                 <h1>Sidebar</h1>
---
+++ src/components/SearchPageContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SearchPageContent.scss';
+
+export default () => <div>
+    <div className="search-box">
+        <input type="text" className="artist-search"/>
+    </div>
+
+    <div className="search-tabs-box">
+        tabs...
+    </div>
+
+    <div className="results-box">
+        results...
+    </div>
+</div>
