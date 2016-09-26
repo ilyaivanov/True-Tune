643954e49e09f42b56529664d96782f07c8ea68d
Ilya Ivanov <static.ila@gmail.com>
2016-09-26 19:12:16 +0300

    Refactor App container

--- src/App.js
+++ src/App.js
@@ -2,48 +2,40 @@ import React from 'react';
 
 import './../node_modules/font-awesome/css/font-awesome.css';
 import './../templates/sound/style/style.scss';
-import debounce from 'lodash/debounce';
 import Player from './components/Player';
-import SearchPage from './components/SearchPage';
-import AlbumsPage from './components/AlbumsPage';
 
-import {findArtists, findAlbums, findTracks} from './services/lastfm';
+import {findTracks} from './services/lastfm';
 
 class App extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            artists: [],
-            artistDetails: null,
-            albums: null,
-            albumDetails: null
+            tracks: []
         };
     }
 
     componentWillMount() {
         //no state, no cache access
-        let {albumName, artistName} = this.props.params;
-        this.setAlbumToPlayer(artistName, albumName);
+        this.setAlbumToPlayer(this.props.params);
     }
 
     componentWillReceiveProps() {
         //state present, cache access
-        let {albumName, artistName} = this.props.params;
-        this.setAlbumToPlayer(artistName, albumName);
+        this.setAlbumToPlayer(this.props.params);
     }
 
-    setAlbumToPlayer(artistName, albumName) {
-        return findTracks(artistName, albumName).then(tracks => this.setState({
-            tracks,
-            albumDetails: {name: albumName}
-        }));
+    setAlbumToPlayer({artistName, albumName}) {
+        return findTracks(artistName, albumName).then(tracks => this.setState({tracks}));
     }
 
     render() {
-        var player = null;
-        if (this.state.albumDetails) {
-            player = <Player tracks={this.state.tracks} album={this.state.albumDetails}
-                             artist={{name: this.props.params.artistName}}/>;
+        let player = null,
+            {albumName, artistName} = this.props.params;
+        if (this.state.tracks) {
+            player = <Player tracks={this.state.tracks}
+                             album={{name: albumName}}
+                             artist={{name: artistName}}/>;
         }
 
         return (
