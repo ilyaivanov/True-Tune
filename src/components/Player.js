21c6daf9d2ac27243b5d3fb535504a3bda9b47a0
Ilya Ivanov <static.ila@gmail.com>
2016-09-26 13:29:23 +0300

    Showing current track name, progress and time. No interactions.
    Made sure current interval id is being disposed on playing a different
    track and and stoping current track. Added a note to include clear
    timeout from component will unmount (need to test that, currently if
    player will mount once - it woun't unmount in the future, but this might
    not be the case for the future)

--- src/components/Player.js
+++ src/components/Player.js
@@ -11,22 +11,25 @@ export default class Player extends React.Component {
         super(props);
         this.state = {
             currentVideo: {},
-            currentTrackId: null,
-            player: null
+            currentTrack: {},
+            currentTime: null,
+            overallTime: null,
+            player: null, //exclude from shouldComponentUpdate
+            currentIntervalId: null //exclude from shouldComponentUpdate,
         };
     }
 
     trackClicked(track) {
-        this.setState({currentTrackId: track.id});
+        this.setState({currentTrack: track});
         findYoutubeVideo(this.props.artist.name, track.name)
-            .then(video => this.setState({currentVideo: video, currentTrackId: track.id}),
-                  error => this.setState({currentTrackId: null}));
+            .then(video => this.setState({currentVideo: video, currentTrack: track}),
+                error => this.setState({currentTrack: {}}));
     }
 
     mapTrack(track, index) {
         return (
             <div key={track.id}
-                 className={classnames('player-item', {active: this.state.currentTrackId == track.id})}
+                 className={classnames('player-item', {active: this.state.currentTrack.id == track.id})}
                  onClick={() => this.trackClicked(track)}>
                 {index + 1}. {track.name}
                 <small>{formatTime(track.duration)}</small>
@@ -37,6 +40,31 @@ export default class Player extends React.Component {
         this.setState({player});
     }
 
+    onPlay() {
+        this.clearCurrentInterval();
+        var currentIntervalId = setInterval(() => this.setState({
+            currentTime: this.state.player.getCurrentTime()
+        }), 1000);
+        this.setState({currentIntervalId, overallTime: this.state.player.getDuration()})
+    }
+
+    //TODO: make sure clearCurrentInterval is called from component will unmount
+    onPause() {
+        this.clearCurrentInterval();
+    }
+
+    clearCurrentInterval() {
+        if (this.state.currentIntervalId) {
+            clearInterval(this.state.currentIntervalId);
+            this.setState({currentIntervalId: null});
+        }
+    }
+
+    onEnd() {
+        //play next song
+        console.log('end');
+    }
+
     render() {
         const opts = {
             height: '150',
@@ -45,17 +73,17 @@ export default class Player extends React.Component {
                 autoplay: 1
             }
         };
-
+        var currentDurationPercent = (this.state.currentTime / this.state.overallTime * 100) + '%';
         return (
             <div>
                 <img src={this.props.album.image}/>
                 <div className="player">
                     <div className="track-progress-container">
-                        <div className="track-progress" style={{width: '33%'}}/>
+                        <div className="track-progress" style={{width: currentDurationPercent || 0}}/>
                     </div>
                     <div style={{textAlign: 'center'}}>
-                        Real &amp; True <br />
-                        <small>0:35 - 3:45</small>
+                        {this.state.currentTrack.name} <br />
+                        <small>{formatTime(this.state.currentTime)} - {formatTime(this.state.overallTime)}</small>
                     </div>
                     <div className="controls">
                         <i className="fa fa-step-backward" aria-hidden="true"/>
@@ -72,7 +100,12 @@ export default class Player extends React.Component {
                     {this.props.tracks.map(this.mapTrack.bind(this))}
                 </div>
                 <div className="player-container">
-                    <Youtube videoId={this.state.currentVideo.id} opts={opts} onReady={e => this.setPlayer(e.target)}/>
+                    <Youtube videoId={this.state.currentVideo.id}
+                             opts={opts}
+                             onReady={e => this.setPlayer(e.target)}
+                             onPlay={this.onPlay.bind(this)}
+                             onEnd={this.onEnd.bind(this)}
+                             onPause={this.onPause.bind(this)}/>
                 </div>
 
             </div>);
