b8db8c71a1c3a73d15682f15149b8d70b1617607
Ilya Ivanov <static.ila@gmail.com>
2016-10-09 18:46:17 +0300

    Implement naive list addition from portlet

--- src/components/ItemPortlet.js
+++ src/components/ItemPortlet.js
@@ -1,30 +1,51 @@
 import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
+import './portlet.scss';
 
-export default function ItemPortlet({item, link, onClick}) {
-    onClick = onClick || (() => {
-        });
-    let child = (
-        <div>
-            <div className="artist-image">
-                <img src={item.image} alt="Foo"/>
-                <div className="shadow"/>
-                <div className="artist-options">
-                    <i className="fa fa-heart-o" aria-hidden="true"/>
-                    <i className="fa fa-ellipsis-h" aria-hidden="true"/>
+export default class ItemPortlet extends React.Component {
+    render() {
+        let {item, link, onClick} = this.props;
+        onClick = onClick || (() => {
+            });
+        let onPlaylistClick = e => {
+            console.log('click');
+            e.stopPropagation();
+        };
+        let onMouseLeave = () => this.dropdown.classList.add('hidden');
+
+        let showPlaylists = e => {
+            this.dropdown.classList.remove('hidden');
+            e.stopPropagation();
+            e.preventDefault();
+        }
+        let child = (
+            <div>
+                <div className="artist-image" onMouseLeave={onMouseLeave}>
+                    <img src={item.image} alt="Foo"/>
+                    <div className="shadow"/>
+                    <div className="dropdown-content hidden" ref={e => this.dropdown = e}>
+                        <button onClick={onPlaylistClick}>Link 1</button>
+                        <br/>
+                        <button onClick={onPlaylistClick}>Link 2</button>
+                        <br/>
+                        <button onClick={onPlaylistClick}>Link 3</button>
+                    </div>
+                    <div className="artist-options">
+                        <i className="dropbtn fa fa-plus" onClick={showPlaylists} aria-hidden="true"/>
+                    </div>
                 </div>
-            </div>
-            <div className="artist-title">{item.name}</div>
-        </div>);
-    if (link) {
-        return (<Link className="grid-item artist-info" to={link} onClick={onClick}>
-            {child}
-        </Link>);
-    }
-    else {
-        return (<div className="grid-item artist-info" onClick={onClick}>
-            {child}
-        </div>);
+                <div className="artist-title">{item.name}</div>
+            </div>);
+        if (link) {
+            return (<Link className="grid-item artist-info" to={link} onClick={onClick}>
+                {child}
+            </Link>);
+        }
+        else {
+            return (<div className="grid-item artist-info" onClick={onClick}>
+                {child}
+            </div>);
+        }
     }
 }
 
