609119ac5070f9ab004ecd41edceba71b3d92f75
Ilya Ivanov <static.ila@gmail.com>
2016-10-11 16:54:30 +0300

    Save store in local storage

--- src/index.js
+++ src/index.js
@@ -11,12 +11,16 @@ require('./favicon.ico'); // Tell webpack to load favicon.ico
 import { syncHistoryWithStore } from 'react-router-redux';
 import './../node_modules/font-awesome/css/font-awesome.css';
 import './../templates/sound/style/style.scss';
+import {loadState, saveState} from './state/store/localStorage';
 
-const store = configureStore();
+const store = configureStore(loadState());
 
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
 
+store.subscribe(() => {
+    saveState(store.getState());
+});
 render(
   <Provider store={store}>
     <Router history={history} routes={routes} />
